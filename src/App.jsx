4cec3f26c2ba7ca1664a1ad6a6a5d693840575e0
Nicolas Urrego <nicurrego@gmail.com>
2025-07-26 14:56:13 +0900

    Class5: Uso de useEffect para Manejar Efectos Secundarios en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
+import CounterWithEffect from './components/CounterWithEffect';
 import NameForm from './components/NameForm';
-import './App.css'
 import Counter from './components/Counter';
 import ToggleButton from './components/ToggleButton';
+
+import './App.css'
 
 function App() {
   const items = ["React", "JavaScript", "Vite"];
 
   return (
-    <section>
-      <h1>!Hola mundo!</h1>
-      <NameForm />
-      <ToggleButton />
-      <Counter />
-      <ul>
-        {items.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </section>
+      <>
+        <section>
+          <h1>!Hola mundo!</h1>
+        <CounterWithEffect />
+          <NameForm />
+          <ToggleButton />
+          <Counter />
+          <ul>
+            {items.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </section>
+      </>
   )
 }
 
---
+++ src/components/CounterWithEffect.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const CounterWithEffect = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log(`El contador cambio a: ${count}`)
+    }, [count])
+
+    return (
+        <div>
+            <p>El contador esta en {count}</p>
+            <button onClick={() => setCount(count + 1)}>Incrementar</button>
+        </div>
+    )
+}
+
+export default CounterWithEffect;
