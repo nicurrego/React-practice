9a33feac8015022eb0574a73f65e7630b1a7eeb2
Nicolas Urrego <nicurrego@gmail.com>
2025-07-26 18:59:10 +0900

    Class8: Instalacion y Uso de Tailwind CSS en Proyectos Web

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import Card from './components/Card';
+import Text from './components/Text/Text';
 import './App.css'
 
 function App() {
-  const items = ["React", "JavaScript", "Vite"];
-
   return (
-    <section>
-      <h1>!Hola mundo!</h1>
-      <Card title="Título de la tarjeta 1" description="Cuerpo de la tarjeta 1" />
-      <Card title="Título de la tarjeta 2" description="Cuerpo de la tarjeta 2" />
-      <ul>
-        {items.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </section>
+    <>
+      <Text />
+    </>
   )
 }
 
---
+++ src/components/Text/Text.jsx
@@ -0,0 +1,9 @@
+const Text = () => {
+    return (
+        <h1 class="text-3xl italic font-bold underline decoration-dotted">
+            Hello world!
+        </h1>
+    )
+}
+
+export default Text
