9c762e55418b27ea0cfc42fb831bbfdb47f3c2dc
Nicolas Urrego <nicurrego@gmail.com>
2025-07-29 10:14:54 +0900

    class17: Componentes con TypeScript en React: Tipado de Props y Funciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import React from 'react';
+import Button from './components/Button';
 import './App.css';
 
-interface AppProps {
-  appName: string;
-}
+function App() {
+  const handleClick = () => alert("Clicked!!!")
 
-const App: React.FC<AppProps> = ({ appName }) => {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to {appName}!</h1>
-        <p>This is your main application component.</p>
-        {/* You can render other components here */}
-        {/* <MyComponent /> */}
-      </header>
+      <Button onclick={handleClick} label="Click me" />
     </div>
   );
-};
+}
 
 export default App;
